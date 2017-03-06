9ee206f4d99d2b1b9a1fd311f5a6fde21405679b
Jonatan Kåhrström <Jonatan Khrstrm>
2017-03-06 21:25:02 +0100

    Fixed run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from skillocate import app
 import os
 
-app.secret_key = os.urandom(24)
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
