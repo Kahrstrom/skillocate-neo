c0d998523011f075edea49d3706a54b191becbf7
Jonatan Kåhrström <Jonatan Khrstrm>
2017-03-07 22:04:56 +0100

    Changed port

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import os
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 33507))
     app.run(host='0.0.0.0', port=port)
