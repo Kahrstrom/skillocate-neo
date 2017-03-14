d950698057894ef595357a272c50c6debac9e19e
Jonatan Kåhrström <Jonatan Khrstrm>
2017-03-14 20:52:36 +0100

    Added all routes

--- skillocate/views.py
+++ skillocate/views.py
@@ -1,7 +1,10 @@
 from .errorhandler import InvalidUsage
 from .models import User, Project, Customer, Tag
-from flask import Flask, request, session, redirect, jsonify, abort
+from flask import Flask, request, session, redirect, jsonify, abort, url_for
 from .services import CustomerService, ProjectService, UserService, EducationService, graph
+from urllib.parse import unquote
+import itertools
+from operator import itemgetter
 
 # Initialize app...
 app = Flask(__name__)
@@ -12,17 +15,20 @@ projectService = ProjectService()
 userService = UserService()
 educationService = EducationService()
 
+# Test route
 @app.route('/api/v1/hello')
 def hello():
     query = "MATCH (node)<-[r]-(related) RETURN node, COLLECT(DISTINCT related) as relations"
     return jsonify(data=graph.data(query))
 
+# Handle invalid requests
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
 
+# Registers a new user
 @app.route('/api/v1/user/register', methods=['POST'])
 def register_user():
     if not userService.register(request):
@@ -30,6 +36,7 @@ def register_user():
     else:
         return jsonify(data='success')
 
+# Handles a login request
 @app.route('/api/v1/user/login', methods=['POST'])
 def login_user():
     # TODO: Bygg denna metoden.
@@ -38,7 +45,7 @@ def login_user():
     else:
         raise InvalidUsage('Invalid password', status_code=401)
 
-
+# Gets user data
 @app.route('/api/v1/user/<username>', methods=['GET'])
 def get_user(username):
     user = userService.get_complete(username)
@@ -47,6 +54,12 @@ def get_user(username):
     else:
         raise InvalidUsage('No such user', status_code=401)
 
+# Updates user data
+@app.route('/api/v1/user/<username>', methods=['PUT'])
+def update_user(username):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Get all user's projects
 @app.route('/api/v1/user/<username>/project', methods=['GET'])
 def get_user_projects(username):
     projects = userService.get_projects(username)
@@ -56,6 +69,7 @@ def get_user_projects(username):
     else:
         raise InvalidUsage('No such user', status_code=404)
 
+# Get all user's educations
 @app.route('/api/v1/user/<username>/education', methods=['GET'])
 def get_user_educations(username):
     educations = userService.get_educations(username)
@@ -65,10 +79,37 @@ def get_user_educations(username):
     else:
         raise InvalidUsage('No such user', status_code=404)
 
+# Get all user's workexperience
+@app.route('/api/v1/user/<username>/workexperience', methods=['GET'])
+def get_user_workexperience(username):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Get all user's certificates
+@app.route('/api/v1/user/<username>/certificate', methods=['GET'])
+def get_user_certificates(username):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Assigns a user to a specific project
 @app.route('/api/v1/user/<username>/project/<id>', methods=['POST'])
 def assign_project(username,id):
     return jsonify(data=userService.assign_project(username, id))
 
+# Unassigns a user from a specific project
+@app.route('/api/v1/user/<username>/project/<id>', methods=['DELETE'])
+def unassign_project(username, id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Creates a workexperience and relates it to a user
+@app.route('/api/v1/user/<username>/workexperience', methods=['POST'])
+def add_user_workexperience(username):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Creates a certificate and relates it to a user
+@app.route('/api/v1/user/<username>/certificate', methods=['POST'])
+def add_user_certificate(username):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Creates an education and relates it to a user
 @app.route('/api/v1/user/<username>/education', methods=['POST'])
 def add_user_education(username):
     education = userService.create_education(username, request)
@@ -77,10 +118,77 @@ def add_user_education(username):
     else:
         raise InvalidUsage('No such user', status_code=404)
 
+# Gets data from a specific education
+@app.route('/api/v1/education/<id>', methods=['GET'])
+def get_education(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Gets data for all educations
+@app.route('/api/v1/education', methods=['GET'])
+def get_all_educations():
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Updates an education
+@app.route('/api/v1/education/<id>', methods=['PUT'])
+def update_education(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Deletes an education
+@app.route('/api/v1/education/<id>', methods=['DELETE'])
+def delete_education(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Gets data from a specific workexperience
+@app.route('/api/v1/workexperience/<id>', methods=['GET'])
+def get_workexperience(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Gets data for all workexperience
+@app.route('/api/v1/workexperience', methods=['GET'])
+def get_all_workexperience():
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Updates a workexperience
+@app.route('/api/v1/workexperience/<id>', methods=['PUT'])
+def update_workexperience(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Deletes a workexperience
+@app.route('/api/v1/workexperience/<id>', methods=['DELETE'])
+def delete_workexperience(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Gets data from a specific certificate
+@app.route('/api/v1/certificate/<id>', methods=['GET'])
+def get_certificate(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Gets data for all certificate
+@app.route('/api/v1/certificate', methods=['GET'])
+def get_all_certificate():
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Updates a certificate
+@app.route('/api/v1/certificate/<id>', methods=['PUT'])
+def update_certificate(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Deletes a certificate
+@app.route('/api/v1/certificate/<id>', methods=['DELETE'])
+def delete_certificate(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Creates a customer
 @app.route('/api/v1/customer', methods=['POST'])
 def create_customer():
     return jsonify(data=customerService.create(request))
 
+# Updates a customer
+@app.route('/api/v1/customer/<id>', methods=['PUT'])
+def update_customer(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Gets data for a specific customer
 @app.route('/api/v1/customer/<id>', methods=['GET'])
 def get_customer(id):
     customer = customerService.get(id)
@@ -89,14 +197,38 @@ def get_customer(id):
     else:
         return jsonify(data=customer)
 
+# Gets all customer data
 @app.route('/api/v1/customer', methods=['GET'])
 def get_customers():
     return jsonify(data=customerService.get_all())
 
-@app.route('/api/v1/project', methods=['POST'])
-def create_project():
-    return jsonify(data=projectService.create(request))
+# Creates a project and relates it to a customer.
+@app.route('/api/v1/customer/<id>/project', methods=['POST'])
+def request_project(id):
+    return jsonify(data=customerService.create_project(id, request))
 
+# REMOVED
+# # Creates a project
+# @app.route('/api/v1/project', methods=['POST'])
+# def create_project():
+#     return jsonify(data=projectService.create(request))
+
+# Updates a project
+@app.route('/api/v1/project/<id>', methods=['PUT'])
+def update_project(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Deletes a project
+@app.route('/api/v1/project/<id>', methods=['DELETE'])
+def delete_project(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Gets data for all projects
+@app.route('/api/v1/project', methods=['GET'])
+def get_projects(id):
+    return jsonify(data=projectService.get_all())
+
+# Gets data for a specific project
 @app.route('/api/v1/project/<id>', methods=['GET'])
 def get_project(id):
     project = projectService.get(id)
@@ -105,18 +237,48 @@ def get_project(id):
     else:
         return jsonify(data=project)
 
+# Sets tags for a specific project. Replaces all current tags.
 @app.route('/api/v1/project/<id>/tag', methods=['POST'])
 def tag_project(id):
     return jsonify(data=projectService.set_tags(id=id, request=request))
 
-@app.route('/api/v1/customer/<id>/tag', methods=['POST'])
+# Sets tags for a specific customer. Replaces all current tags.
+@app.route('/api/v1/customer/<id>/tag', methods=['POST']) 
 def tag_customer(id):
     return jsonify(data=customerService.set_tags(id=id, request=request))
 
+# Sets tags for a specific education. Replaces all current tags.
 @app.route('/api/v1/education/<id>/tag', methods=['POST'])
 def tag_education(id):
     return jsonify(data=educationService.set_tags(id=id, request=request))
 
-@app.route('/api/v1/customer/<id>/project', methods=['POST'])
-def request_project(id):
-    return jsonify(data=customerService.create_project(id, request))
+# Sets tags for a specific workexperience. Replaces all current tags.
+@app.route('/api/v1/workexperience/<id>/tag', methods=['POST'])
+def tag_workexperience(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+# Sets tags for a specific certificate. Replaces all current tags.
+@app.route('/api/v1/certificate/<id>/tag', methods=['POST'])
+def tag_certificate(id):
+    raise InvalidUsage('Not yet implemented', status_code=501)
+
+@app.route('/api/v1/site-map')
+def list_routes():
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(set(rule.methods) - set(['OPTIONS', 'HEAD']))
+        url = url_for(rule.endpoint, **options)
+        line = {'rule': unquote(rule.endpoint), 'methods': unquote(methods), 'url': unquote(url)}
+        output.append(line)
+    
+    output.sort(key=itemgetter("url"))
+    retval = []
+    for key, group in itertools.groupby(output, lambda item: item["url"]):
+        retval.append({'url': key, 'methods': ', '.join([item["methods"] for item in group])})
+
+    return jsonify(data=retval)
